0dafb113968dc5293b3b94ee61cc4324a9dd32ec
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2022-11-23 20:27:40 +0200

    % sura details + verse widget + quran files

--- lib/sura_details/sura_details.dart
+++ lib/sura_details/sura_details.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_c7_sun/sura_details/verse_widget.dart';
 
-class SuraDetailsScreen extends StatelessWidget {
+class SuraDetailsScreen extends StatefulWidget {
   static const String routeName = 'Sura-details';
+
+  @override
+  State<SuraDetailsScreen> createState() => _SuraDetailsScreenState();
+}
+
+class _SuraDetailsScreenState extends State<SuraDetailsScreen> {
+  List<String> verses = [];
 
   @override
   Widget build(BuildContext context) {
     SuraDetailsScreenArgs args =
         (ModalRoute.of(context)?.settings.arguments) as SuraDetailsScreenArgs;
-    print(args.title);
-    print(args.index);
+    if (verses.isEmpty) readFile(args.index);
+
     return Container(
         decoration: const BoxDecoration(
             image: DecorationImage(
@@ -18,7 +27,40 @@ class SuraDetailsScreen extends StatelessWidget {
           appBar: AppBar(
             title: Text(args.title),
           ),
+          body: Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 64),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.separated(
+                itemBuilder: (_, index) {
+                  return VerseWidget(verses[index], index + 1);
+                },
+                separatorBuilder: (_, __) {
+                  return Container(
+                    height: 1,
+                    margin: EdgeInsets.symmetric(horizontal: 24),
+                    width: double.infinity,
+                    color: Theme.of(context).primaryColor,
+                  );
+                },
+                itemCount: verses.length,
+              ),
+            ),
+          ),
         ));
+  }
+
+  void readFile(int index) async {
+    String fileContent =
+        await rootBundle.loadString('assets/files/${index + 1}.txt');
+    List<String> lines = fileContent.trim().split('\n');
+    verses = lines;
+    print(verses);
+    setState(() {});
   }
 }
 
---
+++ lib/sura_details/verse_widget.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class VerseWidget extends StatelessWidget {
+  String text;
+  int index;
+
+  VerseWidget(this.text, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      alignment: Alignment.center,
+      child: Text(
+        '$text {$index}',
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.rtl,
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
